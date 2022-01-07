305f5ba17beeb970ec901d41116c9cdb8b7b35f4
olee2 <65301179+olee2@users.noreply.github.com>
2022-01-07 12:17:48 +0100

    new

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,14 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("change")
     navsub.classList.toggle("nav-change")
 });
+
+const links = document.querySelectorAll(".list-item");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      let current = document.querySelectorAll(".active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  } 
+console.log(links)
