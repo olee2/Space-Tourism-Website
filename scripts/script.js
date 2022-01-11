5622c66f04df92ce005dbcb5209c0952d8b10299
olee2 <65301179+olee2@users.noreply.github.com>
2022-01-11 11:39:43 +0100

    Div

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,20 @@ for (let i = 0; i < links.length; i++) {
     });
   }
 */
+
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
+
+/* Destination NAV */
 const destinations = document.querySelectorAll(".planet-link");
 
 for (let i = 0; i < destinations.length; i++) {
@@ -48,22 +62,11 @@ for (let i = 0; i < destinations.length; i++) {
             image.innerHTML = `<img src="${object.images.png}">`
           }
         }
-    
     });
     });
   }
 
-function readTextFile(file, callback) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.overrideMimeType("application/json");
-  rawFile.open("GET", file, true);
-  rawFile.onreadystatechange = function() {
-      if (rawFile.readyState === 4 && rawFile.status == "200") {
-          callback(rawFile.responseText);
-      }
-  }
-  rawFile.send(null);
-}
+
 
 readTextFile("data.json", function(text){
     var data = JSON.parse(text); //parse JSON
